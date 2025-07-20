e2b8a016b311cd154c5b25afa02d36d699f74f17
Hobaibsajid <hobaibsajid@gmail.com>
2025-07-21 01:10:53 +0500

    modified

--- services/Organizer.js
+++ services/Organizer.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 // Set your correct backend URL here
-const BASE_API_URL = `${process.env.REACT_APP_API_URL}/api/organizers`;
+const BASE_API_URL = "https://shx-backend.onrender.com/api/organizers";
 
 // POST request to register an organizer
 export const registerOrganizer = async (data) => {
